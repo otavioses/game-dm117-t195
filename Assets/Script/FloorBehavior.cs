9fb3af03f0d1222eeb5c4c22463cac5f4fa52e3e
Otávio Souza <otavioses>
2020-08-28 10:06:27 -0300

    Rename some classes

---
+++ Assets/Script/FloorBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorBehavior : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerBehavior>())
+        {
+            collision.rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
